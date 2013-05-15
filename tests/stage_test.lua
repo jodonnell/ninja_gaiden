83fd280e0fec9fd2a94c2bca1d22f8c2d2d319f8
Jacob O'Donnell <jacobodonnell@gmail.com>
2013-05-14 22:36:15 -0400

    fixed broken tests

--- tests/stage_test.lua
+++ tests/stage_test.lua
@@ -3,6 +3,7 @@ module(..., package.seeall)
 function setup()
 	 love.graphics.newImage = function(path) return path end
    stage = Stage(Ninja())
+	 stage.ninja.y = 450
 end
 
 function test_screen_scrolls_when_ninja_in_center()
